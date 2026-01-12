26eb6e2168b87592b4853bbc73600181f34c1a73
Michal Borowiec <michal.borowiec@narf.ai>
2026-01-12 21:10:34 +0100

    project scaffolding

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER "photo-album" SUPERUSER;
+CREATE DATABASE "photo-album" OWNER "photo-album" ENCODING 'utf-8';
